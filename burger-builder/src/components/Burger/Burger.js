33303bc4ffbc3215ea1098d439003ed367a8fef4
AlexandraFishman <alexyafish@gmail.com>
2020-03-25 11:42:04 +0200

    Dynamic render of ingredient list

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -3,11 +3,17 @@ import './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    const ingredientsAsArray = Object.keys(props.ingredients) //turns the keys of the object into an array
+        .map(ingrKey => {
+            return [...Array(props.ingredients[ingrKey])].map((_,i) => 
+                <BurgerIngredient key={ingrKey + i} type={ingrKey}/>);
+        });
+
+    
     return (
         <div className='Burger'>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {ingredientsAsArray}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,23 @@ import Aux from '../../hoc/Aux';
 import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            ingredients: {
+                salad: 1,
+                bacon: 1,
+                cheese: 10,
+                meat: 12
+            }
+        }
+    }
+
+
     render (){
         return (
             <Aux>
-                <Burger/>
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Control to add and  remove ingredients</div>
             </Aux>
         );
