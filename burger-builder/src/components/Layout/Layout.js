ecef63f37f2729aea7a1744500ae5ef1d5b130bd
AlexandraFishman <alexyafish@gmail.com>
2020-03-25 21:01:47 +0200

    Added logo and header - not styled yet

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
 import './Layout.css';
-import '../../assets/burger-king_tomato-king.jpg';
+import burgerLogo from '../../assets/burger-king_tomato-king.jpg';
 
 const layout = (props) => (
     <Aux>
-        <img src="burger-king_tomato-king.jpg"/>
-        <div>Toolbar, Sidedrawr, Backdrop</div>
+        <div className="HeaderWraper">
+            <img src={burgerLogo} alt="Burger Logo" width="150px" height="100px" />
+            <div className="Toolbar">Toolbar, Sidedrawr, Backdrop</div>
+        </div>
         <main className='bodyContent'>
             {props.children}
         </main>
